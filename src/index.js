ffd92c6fc3f128ec00b492c0d9fc3ce0ca63929c
Yura Korniyenko <yura.kornssnko@gmail.com>
2020-09-17 18:41:39 +0300

    Popups added

--- src/index.js
+++ src/index.js
@@ -77,6 +77,14 @@ $(".filters--tabs-btn").on("click", function () {
   });
 
   $(this).addClass("active");
+
+  if ($("#tab-1").hasClass("active")) {
+    $("#select-tab-1").css("display", "block");
+    $("#select-tab-2").css("display", "none");
+  } else {
+    $("#select-tab-2").css("display", "block");
+    $("#select-tab-1").css("display", "none");
+  }
 });
 // <------ Filter's tab change END ------>
 
@@ -98,3 +106,95 @@ $(".input-wrap input").on("blur", function () {
   $(this).parent().removeClass("active");
 });
 // <------ Input wrap change END ------>
+
+// <------ Popup START ------>
+const popupLinks = document.querySelectorAll(".popup--link");
+const body = document.querySelector("body");
+const lockPadding = document.querySelectorAll(".lock-padding");
+const header = document.querySelector(".header-section");
+
+let unlock = true;
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+  for (let i = 0; i < popupLinks.length; i++) {
+    const popupLink = popupLinks[i];
+    popupLink.addEventListener("click", function (e) {
+      const popupName = popupLink.getAttribute("href").replace("#", "");
+      const currentPopup = document.getElementById(popupName);
+      popupOpen(currentPopup);
+      e.preventDefault();
+    });
+  }
+}
+const popupCloseIcon = document.querySelectorAll(".close-popup");
+if (popupCloseIcon.length > 0) {
+  for (let i = 0; i < popupCloseIcon.length; i++) {
+    const el = popupCloseIcon[i];
+    el.addEventListener("click", function (e) {
+      popupClose(el.closest(".popup"));
+      e.preventDefault();
+    });
+  }
+}
+
+function popupOpen(currentPopup) {
+  if (currentPopup && unlock) {
+    const popupActive = document.querySelector(".popup.open");
+    if (popupActive) {
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    currentPopup.classList.add("open");
+    currentPopup.addEventListener("click", function (e) {
+      if (!e.target.closest(".popup--content")) {
+        popupClose(e.target.closest(".popup"));
+      }
+    });
+  }
+}
+function popupClose(popupActive, doUnlock = true) {
+  if (unlock) {
+    popupActive.classList.remove("open");
+    if (doUnlock) {
+      bodyUnlock();
+    }
+  }
+}
+function bodyLock() {
+  const lockPaddingValue =
+    window.innerWidth - document.querySelector("body").offsetWidth + "px";
+  if (lockPadding.length > 0) {
+    for (let i = 0; i < lockPadding.length; i++) {
+      const el = lockPadding[i];
+      el.style.paddingRight = lockPaddingValue;
+    }
+  }
+  header.style.paddingRight = lockPaddingValue;
+  body.style.paddingRight = lockPaddingValue;
+  body.classList.add("lock");
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+function bodyUnlock() {
+  setTimeout(function () {
+    for (let i = 0; i < lockPadding.length; i++) {
+      const el = lockPadding[i];
+      el.style.paddingRight = "0px";
+    }
+    body.style.paddingRight = "0px";
+    header.style.paddingRight = "0px";
+    body.classList.remove("lock");
+  }, timeout);
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+// <------ Popup END ------>
+
